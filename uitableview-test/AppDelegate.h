90556e542ce296032c339d019873895f4393ad20
svram <vikrambahl@svram.local>
2014-12-23 18:11:07 +0800

    Initial Commit

---
+++ uitableview-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uitableview-test
+//
+//  Created by Vikram Bahl on 23/12/14.
+//  Copyright (c) 2014 Vikram Bahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ uitableview-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uitableview-test
+//
+//  Created by Vikram Bahl on 23/12/14.
+//  Copyright (c) 2014 Vikram Bahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
