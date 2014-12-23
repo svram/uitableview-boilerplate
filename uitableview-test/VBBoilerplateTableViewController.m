b4acb74adf9aa0dd7853fd4e9ac278198ecbd448
svram <vikrambahl@svram.local>
2014-12-23 18:35:38 +0800

    final commit

--- uitableview-test/VBBoilerplateTableViewController.m
+++ uitableview-test/VBBoilerplateTableViewController.m
@@ -32,26 +32,50 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
+//#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
+//#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if (section == 0) {
+        return 2;
+    }
+    else if (section == 1)
+    {
+        return 1;
+    }
+    else{
+        return 3;
+    }
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
+    }
+    else if (indexPath.section == 1)
+    {
+        cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
+    }
+    else{
+        cell.textLabel.text = [NSString stringWithFormat:@"Cell %li", (long)indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
+    }
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
